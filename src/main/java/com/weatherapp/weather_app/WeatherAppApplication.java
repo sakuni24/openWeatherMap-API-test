8cfd985605cac4c2fa809210eddadc4c001cf2dd
Sakuni_Herath_Gallage <saku9706@gmail.com>
2024-12-13 15:36:19 +0530

    added caching

--- src/main/java/com/weatherapp/weather_app/WeatherAppApplication.java
+++ src/main/java/com/weatherapp/weather_app/WeatherAppApplication.java
@@ -2,8 +2,10 @@ package com.weatherapp.weather_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class WeatherAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/weatherapp/weather_app/service/WeatherService.java
+++ src/main/java/com/weatherapp/weather_app/service/WeatherService.java
@@ -1,6 +1,7 @@
 package com.weatherapp.weather_app.service;
 
 import com.weatherapp.weather_app.model.WeatherResponse;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,6 +11,7 @@ public class WeatherService {
     private final String API_KEY = "e5e2976d63454f55be7c53753aed0c06";
     private final String BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q=";
 
+    @Cacheable("weather")
     public WeatherResponse getWeatherByCity(String city) {
         String url = BASE_URL + city + "&appid=" + API_KEY;
         RestTemplate restTemplate = new RestTemplate();
